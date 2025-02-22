1335d128030798952d65ff0d7581ff55a553fde2
sharif57 <sharifmahamud577951@gmail.com>
2025-02-22 21:56:57 +0600

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,52 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     rules: {
+//       "@typescript-eslint/no-explicit-any": "off"
+//     }
+//   }
+// ];
+
+// export default eslintConfig;
+
+
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
-  }
+      eqeqeq: "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "no-const-assign": "warn",
+      "no-console": "warn",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: [".node_modules/*"]
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
-
-export default eslintConfig;
